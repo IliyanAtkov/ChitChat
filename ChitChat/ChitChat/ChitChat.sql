59f42266b8cd3bab32375dc5b712696d83e74525
OzoneBG <muzunov875@gmail.com>
2014-07-23 17:54:27 +0300

    sql file
    Just created the firls SQL query for the user

---
+++ ChitChat/ChitChat/ChitChat.sql
@@ -0,0 +1,17 @@
+/** Users table */
+CREATE TABLE users (
+	id int(11) NOT_NULL AUTO_INCREMENT,
+	username varchar(30) NOT_NULL,
+	password char(30) NOT_NULL,
+	email varchar(90),
+	joinDate DATE,
+	info tinytext DEFAULT NULL,
+	city varchar(20) DEFAULT NULL,
+	nation varchar(20) DEFAULT NULL,
+	phone int(16) DEFAUL NULL,
+	sex ENUM('Male','Female', 'Unknown') DEFAULT 'Unknown',
+	name varchar(100) DEFAULT NULL,
+	isDonator tinyint(1) NOT NULL,
+	onlineStance ENUM('Online', 'Busy', 'AFK', 'Ghost', 'Offline'),
+	PRIMARY KEY(id)
+);
