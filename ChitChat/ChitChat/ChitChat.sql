0ee64c323fb599d3b6141879a2c06dce62c5ebd5
OzoneBG <muzunov875@gmail.com>
2014-07-23 18:45:10 +0300

    Totaly updated the users query

--- ChitChat/ChitChat/ChitChat.sql
+++ ChitChat/ChitChat/ChitChat.sql
@@ -1,18 +1,18 @@
 /** Users table */
-CREATE TABLE users (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	username varchar(30) NOT NULL,
-	password char(30) NOT NULL,
-	email varchar(90),
-	joinDate DATE NOT NULL,
-	ip varchar(15) NOT NULL,
-	info tinytext DEFAULT NULL,
-	city varchar(20) DEFAULT NULL,
-	nation varchar(20) DEFAULT NULL,
-	phone int(16) DEFAULT NULL,
-	sex ENUM('Male','Female', 'Unknown') DEFAULT 'Unknown',
-	name varchar(100) DEFAULT NULL,
-	isDonator tinyint(1) NOT NULL,
-	onlineStance ENUM('Online', 'Busy', 'AFK', 'Ghost', 'Offline'),
-	PRIMARY KEY(id)
-);
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) COLLATE utf8_bin NOT NULL,
+  `password` char(30) COLLATE utf8_bin NOT NULL,
+  `email` varchar(90) COLLATE utf8_bin DEFAULT NULL,
+  `joinDate` date NOT NULL,
+  `ip` varchar(15) COLLATE utf8_bin NOT NULL,
+  `info` tinytext COLLATE utf8_bin,
+  `city` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `nation` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `phone` int(16) DEFAULT NULL,
+  `sex` enum('Male','Female','Unknown') COLLATE utf8_bin DEFAULT 'Unknown',
+  `name` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `isDonator` tinyint(1) NOT NULL,
+  `onlineStance` enum('Online','Busy','AFK','Ghost','Offline') COLLATE utf8_bin NOT NULL DEFAULT 'Offline',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
