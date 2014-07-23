249e0e4f4e2f8f314bd7f35ca6e2d94cd55138fb
OzoneBG <muzunov875@gmail.com>
2014-07-23 18:01:15 +0300

    Finally made it working, finished the users table

--- ChitChat/ChitChat/ChitChat.sql
+++ ChitChat/ChitChat/ChitChat.sql
@@ -1,15 +1,15 @@
 /** Users table */
 CREATE TABLE users (
 	id int(11) NOT NULL AUTO_INCREMENT,
-	username varchar(30) NOT_NULL,
-	password char(30) NOT_NULL,
+	username varchar(30) NOT NULL,
+	password char(30) NOT NULL,
 	email varchar(90),
 	joinDate DATE NOT NULL,
 	ip varchar(15) NOT NULL,
 	info tinytext DEFAULT NULL,
 	city varchar(20) DEFAULT NULL,
 	nation varchar(20) DEFAULT NULL,
-	phone int(16) DEFAUL NULL,
+	phone int(16) DEFAULT NULL,
 	sex ENUM('Male','Female', 'Unknown') DEFAULT 'Unknown',
 	name varchar(100) DEFAULT NULL,
 	isDonator tinyint(1) NOT NULL,
