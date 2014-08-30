6a703a3fcb28cf0401aec162e6e3d7137f033bfa
OzoneBG <muzunov875@gmail.com>
2014-08-30 12:18:15 +0300

    das

---
+++ ChitChat/WriteToFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WriteToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer = new StreamWriter(null, UTF8Encoding);
+
+            string path = "test.txt";
+            int ln = 1;
+            string writing = "App started..";
+
+            using (writer)
+            {
+                writer.WriteLine(writing, path);
+            }
+        }
+    }
+}
